78fde4a04c076892da99654d625db9e419035d74
Grant Sorbo <gtsorbo@google.com>
2021-10-26 18:06:50 -0500

    CSR test

--- test/integration/app_cicd/app_cicd_test.go
+++ test/integration/app_cicd/app_cicd_test.go
@@ -34,6 +34,9 @@ func TestAppCICDExample(t *testing.T) {
 	const triggerName       = "app-source-trigger"
 	const garRepoNameSuffix = "app-image-repo"
 	const primaryLocation   = "us-central1"
+	const appSourceRepoName = "app-source"
+	const manifestDryRepoName = "app-dry-manifests"
+	const manifestWetRepoName = "app-wet-manifests"
 
 	// initialize Terraform test from the Blueprints test framework
 	appCICDT := tft.NewTFBlueprintTest(t)
@@ -47,23 +50,45 @@ func TestAppCICDExample(t *testing.T) {
 		// the tft struct can be used to pull output variables of the TF module being invoked by this test and use the op object (a gjson struct)
 		// to parse through the JSON results and assert the values of the resource against the constants defined above
 
+		projectID := appCICDT.GetStringOutput("project_id")
+
 		// Cloud Build Trigger - App Source
-		gcb := gcloud.Run(t, fmt.Sprintf("beta builds triggers describe %s --project %s", triggerName, appCICDT.GetStringOutput("project_id")))
+		gcb := gcloud.Run(t, fmt.Sprintf("beta builds triggers describe %s --project %s", triggerName, projectID))
 
 		assert.Equal(triggerName, gcb.Get("name").String(), "Cloud Build Trigger name is valid")
-		assert.Equal("app-dry-manifests", gcb.Get("substitutions._MANIFEST_DRY_REPO").String(), "Manifest Dry Repo trigger substitution is valid")
-		assert.Equal("app-wet-manifests", gcb.Get("substitutions._MANIFEST_WET_REPO").String(), "Manifest Wet Repo trigger substitution is valid")
+		assert.Equal(manifestDryRepoName, gcb.Get("substitutions._MANIFEST_DRY_REPO").String(), "Manifest Dry Repo trigger substitution is valid")
+		assert.Equal(manifestWetRepoName, gcb.Get("substitutions._MANIFEST_WET_REPO").String(), "Manifest Wet Repo trigger substitution is valid")
 		assert.Contains(gcb.Get("substitutions._DEFAULT_REGION").String(), primaryLocation, "Default Region trigger substitution is valid")
-		assert.Equal("app-source", gcb.Get("triggerTemplate.repoName").String(), "Attached CSR repo is valid")
+		assert.Equal(appSourceRepoName, gcb.Get("triggerTemplate.repoName").String(), "Attached CSR repo is valid")
 
 		// Artifact Registry repository
-		gar := gcloud.Run(t, fmt.Sprintf("artifacts repositories describe %s-%s --project %s --location %s", appCICDT.GetStringOutput("project_id"), garRepoNameSuffix, appCICDT.GetStringOutput("project_id"), primaryLocation))
-		garFullname := "projects/" + appCICDT.GetStringOutput("project_id") + "/locations/" + primaryLocation + "/repositories/" + appCICDT.GetStringOutput("project_id") + "-" + garRepoNameSuffix
+		gar := gcloud.Run(t, fmt.Sprintf("artifacts repositories describe %s-%s --project %s --location %s", projectID, garRepoNameSuffix, projectID, primaryLocation))
+		garFullname := "projects/" + projectID + "/locations/" + primaryLocation + "/repositories/" + projectID + "-" + garRepoNameSuffix
 		assert.Equal(garFullname, gar.Get("name").String(), "GAR Repo is valid")
 
 		// TODO: BinAuthz
 
 		// TODO: CSR
+		csrSource := gcloud.Run(t, fmt.Sprintf("source repos describe %s --project %s", appSourceRepoName, projectID))
+		csrDryManifest := gcloud.Run(t, fmt.Sprintf("source repos describe %s --project %s", manifestDryRepoName, projectID))
+		csrWetManifest := gcloud.Run(t, fmt.Sprintf("source repos describe %s --project %s", manifestWetRepoName, projectID))
+		
+		csrSourceFullName := "projects/" + projectID + "/repos/" + appSourceRepoName
+		csrDryManifestFullName := "projects/" + projectID + "/repos/" + manifestDryRepoName
+		csrWetManifestFullName := "projects/" + projectID + "/repos/" + manifestWetRepoName
+
+		csrSourceURL := "https://source.developers.google.com/p/" + projectID + "/r/" + appSourceRepoName
+		csrDryManifestURL := "https://source.developers.google.com/p/" + projectID + "/r/" + manifestDryRepoName
+		csrWetManifestURL := "https://source.developers.google.com/p/" + projectID + "/r/" + manifestWetRepoName
+
+		assert.Equal(csrSourceFullName, csrSource.Get("name").String(), "CSR App Source repo name is valid")
+		assert.Equal(csrDryManifestFullName, csrDryManifest.Get("name").String(), "CSR Dry Manifest repo name is valid")
+		assert.Equal(csrWetManifestFullName, csrWetManifest.Get("name").String(), "CSR Wet Manifest repo name is valid")
+
+		assert.Equal(csrSourceURL, csrSource.Get("url").String(), "CSR App Source URL is valid")
+		assert.Equal(csrDryManifestURL, csrDryManifest.Get("url").String(), "CSR Dry Manifest URL is valid")
+		assert.Equal(csrWetManifestURL, csrWetManifest.Get("url").String(), "CSR Wet Manifest URL is valid")
+
 
 
 	})
