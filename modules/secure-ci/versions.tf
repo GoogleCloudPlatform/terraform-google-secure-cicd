68e59a5774bc5327687d67e6855d3ef532676375
Grant Sorbo <gtsorbo@google.com>
2021-12-16 17:31:55 -0600

    version fix

--- modules/secure-ci/versions.tf
+++ modules/secure-ci/versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.45"
+      version = ">= 3.45"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 3.45"
+      version = ">= 3.45"
     }
   }
 
