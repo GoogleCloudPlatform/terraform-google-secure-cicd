af55f21ca2799e0db04c0b45adddd7cb88b44766
Grant Sorbo <gtsorbo@google.com>
2022-01-11 13:48:16 -0600

    update tests

--- test/fixtures/cloudbuild_private_pool/outputs.tf
+++ test/fixtures/cloudbuild_private_pool/outputs.tf
@@ -14,3 +14,7 @@
  * limitations under the License.
  */
 
+output "project_id" {
+  description = "The ID of the project in which resources are provisioned."
+  value       = var.project_id
+}
--- test/integration/cloudbuild_private_pool/cloudbuild_private_pool_test.go
+++ test/integration/cloudbuild_private_pool/cloudbuild_private_pool_test.go
@@ -32,13 +32,6 @@ import (
 func TestCloudBuildPrivatePoolExample(t *testing.T) {
 	// define constants for all required assertions in the test case
 
-	// const sourceTriggerName    = "app-source-trigger"
-	// const garRepoNameSuffix    = "app-image-repo"
-	// const primaryLocation      = "us-central1"
-	// const appSourceRepoName    = "app-source"
-	// const manifestDryRepoName  = "app-dry-manifests"
-	// const manifestWetRepoName  = "app-wet-manifests"
-
 	// initialize Terraform test from the Blueprints test framework
 	cloudBuildPrivatePoolT := tft.NewTFBlueprintTest(t)
 
@@ -51,7 +44,25 @@ func TestCloudBuildPrivatePoolExample(t *testing.T) {
 		// the tft struct can be used to pull output variables of the TF module being invoked by this test and use the op object (a gjson struct)
 		// to parse through the JSON results and assert the values of the resource against the constants defined above
 
-		// projectID := cloudBuildPrivatePoolT.GetStringOutput("project_id")
+		projectID := cloudBuildPrivatePoolT.GetStringOutput("project_id")
+
+		// Worker Pool
+		workerPoolAddress := gcloud.Run(t, fmt.Sprintf("compute addresses describe worker-pool-range --global"))
+		assert.Equal("10.37.0.0", workerPoolAddress.Get("address").String(), fmt.Sprintf("Worker pool address range is 10.37.0.0")
+		assert.Equal("INTERNAL", workerPoolAddress.Get("addressType").String(), fmt.Sprintf("Worker pool address range is type INTERNAL")
+		assert.Equal("VPC_PEERING", workerPoolAddress.Get("purpose").String(), fmt.Sprintf("Worker pool address range is for VPC_PEERING")
+
+		privatePoolVPC := gcloud.Run(t, fmt.Sprintf("compute networks describe gke-private-pool-example-vpc"))
+
+		privatePool := gcloud.Run(t, fmt.Sprintf("builds worker-pools describe cloudbuild-private-worker-pool --region=us-central1"))
+		assert.Equal("RUNNING", privatePool.Get("state").String(), fmt.Sprintf("Worker pool is RUNNING")
+
+		// VPN Tunnels
+		vpnTunnels := [6]string{"cloudbuild-to-gke-private-vpc-dev-remote-0", "cloudbuild-to-gke-private-vpc-dev-remote-1", "cloudbuild-to-gke-private-vpc-prod-remote-0", "cloudbuild-to-gke-private-vpc-prod-remote-1", "cloudbuild-to-gke-private-vpc-qa-remote-0", "cloudbuild-to-gke-private-vpc-qa-remote-1"}
+		for _, tunnel := range vpnTunnels {
+			tunnelStatus := gcloud.Run(t, fmt.Sprintf("compute vpn-tunnels describe %s --region=us-central1", tunnel))
+			assert.Equal("Tunnel is up and running.", tunnelStatus.Get("detailedStatus").String(), fmt.Sprintf("%s is created and running", tunnel))
+		}
 
 	})
 	// call the test function to execute the integration test
