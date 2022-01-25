3006599b2ca7ee4d1ea90edc9dbc2e353e7f6990
Grant Sorbo <gtsorbo@google.com>
2022-01-24 18:17:33 -0600

    formatting

--- examples/gke_cloudbuild_private_pool/main.tf
+++ examples/gke_cloudbuild_private_pool/main.tf
@@ -29,22 +29,21 @@ module "cloudbuild_private_pool" {
 
 locals {
   gke_networks = [
-    for net in var.gke_networks : 
-      merge(net, local.vpn_config[net.network])
+    for net in var.gke_networks : merge(net, local.vpn_config[net.network])
   ]
   vpn_config = {
-    gke-private-vpc-dev  = {
+    gke-private-vpc-dev = {
       gateway_1_asn = 65001,
       gateway_2_asn = 65002,
       bgp_range_1   = "169.254.1.0/30",
       bgp_range_2   = "169.254.2.0/30"
-    }
-    gke-private-vpc-qa   = {
+    },
+    gke-private-vpc-qa = {
       gateway_1_asn = 65003,
       gateway_2_asn = 65004,
       bgp_range_1   = "169.254.3.0/30",
       bgp_range_2   = "169.254.4.0/30"
-    }
+    },
     gke-private-vpc-prod = {
       gateway_1_asn = 65005,
       gateway_2_asn = 65006,
@@ -55,7 +54,7 @@ locals {
 }
 
 module "gke_cloudbuild_vpn" {
-  for_each  = local.gke_networks
+  for_each = local.gke_networks
 
   source = "../../modules/workerpool-gke-ha-vpn"
 
--- examples/private_cluster_cicd/main.tf
+++ examples/private_cluster_cicd/main.tf
@@ -66,65 +66,51 @@ module "cloudbuild_private_pool" {
 }
 
 # Cloud Build Workerpool <-> GKE HA VPNs
-module "gke_cloudbuild_vpn_0" {
-  source = "../../modules/workerpool-gke-ha-vpn"
-
-  project_id = var.project_id
-  location   = "us-central1"
-
-  gke_project             = var.gke_networks[0].project_id
-  gke_network             = var.gke_networks[0].network
-  gke_location            = var.gke_networks[0].location
-  gke_control_plane_cidrs = var.gke_networks[0].control_plane_cidrs
-
-  workerpool_network = module.cloudbuild_private_pool.workerpool_network
-  workerpool_range   = module.cloudbuild_private_pool.workerpool_range
-  gateway_1_asn      = 65001
-  gateway_2_asn      = 65002
-  bgp_range_1        = "169.254.1.0/30"
-  bgp_range_2        = "169.254.2.0/30"
-
-  vpn_router_name_prefix = "pc-"
+locals {
+  gke_networks = [
+    for net in var.gke_networks : merge(net, local.vpn_config[net.network])
+  ]
+  vpn_config = {
+    gke-private-vpc-dev = {
+      gateway_1_asn = 65001,
+      gateway_2_asn = 65002,
+      bgp_range_1   = "169.254.1.0/30",
+      bgp_range_2   = "169.254.2.0/30"
+    },
+    gke-private-vpc-qa = {
+      gateway_1_asn = 65003,
+      gateway_2_asn = 65004,
+      bgp_range_1   = "169.254.3.0/30",
+      bgp_range_2   = "169.254.4.0/30"
+    },
+    gke-private-vpc-prod = {
+      gateway_1_asn = 65005,
+      gateway_2_asn = 65006,
+      bgp_range_1   = "169.254.5.0/30",
+      bgp_range_2   = "169.254.6.0/30"
+    }
+  }
 }
 
-module "gke_cloudbuild_vpn_1" {
+module "gke_cloudbuild_vpn" {
+  for_each = local.gke_networks
+
   source = "../../modules/workerpool-gke-ha-vpn"
 
   project_id = var.project_id
   location   = "us-central1"
 
-  gke_project             = var.gke_networks[1].project_id
-  gke_network             = var.gke_networks[1].network
-  gke_location            = var.gke_networks[1].location
-  gke_control_plane_cidrs = var.gke_networks[1].control_plane_cidrs
+  gke_project             = each.value.project_id
+  gke_network             = each.value.network
+  gke_location            = each.value.location
+  gke_control_plane_cidrs = each.value.control_plane_cidrs
 
   workerpool_network = module.cloudbuild_private_pool.workerpool_network
   workerpool_range   = module.cloudbuild_private_pool.workerpool_range
-  gateway_1_asn      = 65003
-  gateway_2_asn      = 65004
-  bgp_range_1        = "169.254.3.0/30"
-  bgp_range_2        = "169.254.4.0/30"
-
-  vpn_router_name_prefix = "pc-"
-}
-
-module "gke_cloudbuild_vpn_2" {
-  source = "../../modules/workerpool-gke-ha-vpn"
-
-  project_id = var.project_id
-  location   = "us-central1"
-
-  gke_project             = var.gke_networks[2].project_id
-  gke_network             = var.gke_networks[2].network
-  gke_location            = var.gke_networks[2].location
-  gke_control_plane_cidrs = var.gke_networks[2].control_plane_cidrs
-
-  workerpool_network = module.cloudbuild_private_pool.workerpool_network
-  workerpool_range   = module.cloudbuild_private_pool.workerpool_range
-  gateway_1_asn      = 65005
-  gateway_2_asn      = 65006
-  bgp_range_1        = "169.254.5.0/30"
-  bgp_range_2        = "169.254.6.0/30"
+  gateway_1_asn      = each.value.gateway_1_asn
+  gateway_2_asn      = each.value.gateway_2_asn
+  bgp_range_1        = each.value.bgp_range_1
+  bgp_range_2        = each.value.bgp_range_2
 
   vpn_router_name_prefix = "pc-"
 }
