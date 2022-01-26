eb7a2600d938d7ed48aa777ddc8e186ccbe9a064
Grant Sorbo <gtsorbo@google.com>
2022-01-25 21:37:45 -0600

    different workerpool vpc names for examples

--- examples/cloudbuild_private_pool/main.tf
+++ examples/cloudbuild_private_pool/main.tf
@@ -22,7 +22,7 @@ module "cloudbuild_private_pool" {
   location                  = var.primary_location
   worker_pool_name          = "cloudbuild-private-worker-pool"
   create_cloudbuild_network = true
-  private_pool_vpc_name     = "gke-private-pool-example-vpc"
+  private_pool_vpc_name     = "workerpool-example-vpc"
   worker_address            = "10.37.0.0"
   worker_range_name         = "gke-private-pool-worker-range"
 }
