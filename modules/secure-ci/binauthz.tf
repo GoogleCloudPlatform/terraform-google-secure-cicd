8db06fd1e79bde1da8a800555a013408eab3ef94
Grant Sorbo <gtsorbo@google.com>
2022-05-25 12:46:22 -0500

    update binauthz module

--- modules/secure-ci/binauthz.tf
+++ modules/secure-ci/binauthz.tf
@@ -32,9 +32,8 @@ resource "google_kms_key_ring" "keyring" {
 }
 
 module "attestors" {
-  # temporarily refer to PR for provider version compatibility
-  source = "github.com/terraform-google-modules/terraform-google-kubernetes-engine//modules/binary-authorization?ref=binauthz-services"
-  #version  = "~> 17.3"
+  source = "terraform-google-modules/kubernetes-engine/google//modules/binary-authorization"
+  version  = "~> 21.0"
   for_each = toset(var.attestor_names_prefix)
 
   project_id    = var.project_id
