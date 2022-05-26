a5ac5e5b541d4823cb979ccd3af29e01b52da53f
Grant Sorbo <gtsorbo@google.com>
2022-05-26 11:29:14 -0500

    formatting

--- modules/secure-ci/binauthz.tf
+++ modules/secure-ci/binauthz.tf
@@ -32,7 +32,7 @@ resource "google_kms_key_ring" "keyring" {
 }
 
 module "attestors" {
-  source = "terraform-google-modules/kubernetes-engine/google//modules/binary-authorization"
+  source   = "terraform-google-modules/kubernetes-engine/google//modules/binary-authorization"
   version  = "~> 21.0"
   for_each = toset(var.attestor_names_prefix)
 
