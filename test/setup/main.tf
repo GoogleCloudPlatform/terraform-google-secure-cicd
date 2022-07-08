51912498ef57f8bc77a19b3eae774662f8c13a53
Grant Sorbo <gtsorbo@google.com>
2022-07-08 14:51:46 -0500

    upgrade test cluster to containerd

--- test/setup/main.tf
+++ test/setup/main.tf
@@ -173,7 +173,7 @@ module "gke_cluster" {
       local_ssd_count   = 0
       disk_size_gb      = 100
       disk_type         = "pd-standard"
-      image_type        = "COS"
+      image_type        = "COS_CONTAINERD"
       auto_repair       = true
       auto_upgrade      = true
       preemptible       = false
