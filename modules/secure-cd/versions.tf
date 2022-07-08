cf14790ad93cfd41111a9ceafe4898dfaf6a857e
Grant Sorbo <gtsorbo@google.com>
2022-07-08 15:32:26 -0500

    update attestor upstream

--- modules/secure-cd/versions.tf
+++ modules/secure-cd/versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.27, < 5.0"
+      version = ">= 4.22, < 5.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 4.27, < 5.0"
+      version = ">= 4.22, < 5.0"
     }
   }
 
