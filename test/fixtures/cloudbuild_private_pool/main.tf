7d280545b84a1ca6fa7482a4587fbf8d163ad344
Grant Sorbo <gtsorbo@google.com>
2022-08-10 20:24:24 +0000

    modified IP range for workerpool

--- test/fixtures/cloudbuild_private_pool/main.tf
+++ test/fixtures/cloudbuild_private_pool/main.tf
@@ -76,24 +76,6 @@ module "example" {
   ])
 }
 
-
-/////////
-/**
- * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 ###### Private Clusters ######
 # Private Cluster VPCs
 module "vpc_private_cluster" {
@@ -174,7 +156,7 @@ module "gke_private_cluster" {
       display_name = "VPC"
     },
     {
-      cidr_block   = "10.39.0.0/16"
+      cidr_block   = "10.37.0.0/16"
       display_name = "CLOUDBUILD"
     }
   ]
