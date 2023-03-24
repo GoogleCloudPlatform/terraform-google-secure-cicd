9e88ea89099ca7fe161e24af752c6ba8d721e7f1
Grant Sorbo <gtsorbo@google.com>
2023-03-24 00:29:22 +0000

    feat!: remove VPN

--- examples/standalone_single_project/network.tf
+++ examples/standalone_single_project/network.tf
@@ -60,30 +60,3 @@ resource "google_compute_network_peering_routes_config" "gke_peering_routes_conf
   import_custom_routes = true
   export_custom_routes = true
 }
-
-# Cloud Build Workerpool <-> GKE HA VPNs
-module "gke_cloudbuild_vpn" {
-  source  = "GoogleCloudPlatform/secure-cicd/google//modules/workerpool-gke-ha-vpn"
-  version = "~> 0.3"
-
-  project_id = var.project_id
-  location   = var.region
-
-  gke_project  = var.project_id
-  gke_network  = module.vpc.network_name
-  gke_location = var.region
-  gke_control_plane_cidrs = {
-    (module.gke_cluster[var.env1_name].master_ipv4_cidr_block) = "GKE ${var.env1_name} control plane"
-    (module.gke_cluster[var.env2_name].master_ipv4_cidr_block) = "GKE ${var.env2_name} control plane",
-    (module.gke_cluster[var.env3_name].master_ipv4_cidr_block) = "GKE ${var.env3_name} control plane",
-  }
-
-  workerpool_network = module.cloudbuild_private_pool.workerpool_network
-  workerpool_range   = module.cloudbuild_private_pool.workerpool_range
-  gateway_1_asn      = 65001
-  gateway_2_asn      = 65002
-  bgp_range_1        = "169.254.1.0/30"
-  bgp_range_2        = "169.254.2.0/30"
-
-  labels = var.labels
-}
